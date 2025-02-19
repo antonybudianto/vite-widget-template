465661f106de88cd0a09a9c48342c767d49d8084
Antony Budianto <antonybudianto@gmail.com>
2025-02-19 09:20:46 +0700

    add sample

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="hello">Hello</div>
+  <div class="hello">
+    <h2>Hello Vue 3 Lib!</h2>
+    <div>counter: {{ counter }}</div>
+    <button @click="() => counter++">inc++</button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref, version } from "vue";
+
+const counter = ref(0);
 
 onMounted(() => {
-  console.log("mounted");
+  console.log("mounted: ", version);
 });
 </script>
 
